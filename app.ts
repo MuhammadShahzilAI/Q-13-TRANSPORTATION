3421d6c1483052232262abdd2af3cb3d1f0d0945
MUHAMMAD SHAHZIL <164368568+MuhammadShahzilAI@users.noreply.github.com>
2024-03-22 16:11:17 +0500

    Q 13 TRANSPORTATION
    ASSIGMENT OF GOVERNER SINDH

---
+++ app.ts
@@ -0,0 +1,6 @@
+//exercise 13
+ let transportation : string[] =[`car`,`Bus`,`Bike`];
+  for(let i=0;i<transportation.length; i++) {
+    console.log(`I would like to own a `+ transportation[i]);
+
+}
